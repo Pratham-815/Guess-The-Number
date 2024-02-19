3fbe64c4707140882aa0e01bd77fff75e779aaa0
Pratham Agarwal <143127428+Pratham-815@users.noreply.github.com>
2024-02-19 07:37:00 +0000

    Function to compare both numbers

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import random
 
+def check_answer(guess, answer):
+    """Fuction to check user guess against actual answer"""
+
+    if guess > answer:
+        print("Too high!!!")
+    elif guess < answer:
+        print("Too low!!!")
+    else:
+        print(f"You got it!!! The answer was {answer}")
+
 print("Welcome To The Number Guessing Game!!!")
 print("I am thinking of a number between 1 to 100")
 
